d431f00f8887c517fd3b8e2459be6aa8bfc05a68
boffskbob <acornbear03@gmail.com>
2024-12-21 02:26:11 -0500

    add drag and drop functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const DIAMETER = 50;
+const DEFAULT_DIAMETER = 50;
 
 
 function App() {
@@ -13,29 +13,48 @@ function App() {
   const handleClick = (e) => {
     console.log("ran handle click");
     const sandbox = document.getElementById("sandbox").getBoundingClientRect();
-    const x = e.clientX - sandbox.left - DIAMETER / 2; // Offset for the item size
-    const y = e.clientY - sandbox.top - DIAMETER / 2;
+    const x = e.clientX - sandbox.left - DEFAULT_DIAMETER / 2; 
+    const y = e.clientY - sandbox.top - DEFAULT_DIAMETER / 2;
 
     // Add a new draggable item
     setCount(count + 1);
     setItems([...items, { id: count, x, y }]);
   }
 
-  const handleDrag = (e) => {
-    const item = e.target;
-    console.log(e.id);
+  const handleDrag = (e, id) => {
+    // const item = document.getElementById(id);
+    console.log("start drag");
+    e.dataTransfer.setData("item-id", id);
+  }
+
+  const handleDrop = (e) => {
+    console.log("start drop");
+    const itemId = e.dataTransfer.getData("item-id");
+    const item = document.getElementById(itemId);
+    
+    const sandbox = document.getElementById("sandbox").getBoundingClientRect();
+    const x = e.clientX - sandbox.left - DEFAULT_DIAMETER / 2; 
+    const y = e.clientY - sandbox.top - DEFAULT_DIAMETER / 2;
+
+    item.setAttribute("style", "left: " + String(x) + "px; top: " + String(y) + "px");
+    console.log(e.clientX, e.clientY);
+  }
+  
+  const handleDragOver = (e) => {
+    e.preventDefault();
   }
 
   return (
     <>
-      <div id='sandbox' onClick={handleClick}>
+      <div id='sandbox' onClick={handleClick} onDrop={handleDrop} onDragOver={handleDragOver}>
       {items.map((item) => (
         <div
           className='lens'
+          id={item.id}
           key={item.id}
           style={{ left: item.x, top: item.y }}
           draggable
-          onDrag={(e) => handleDrag(e, item.id)}
+          onDragStart={(e) => handleDrag(e, item.id)}
         ></div>
       ))}
       </div>
